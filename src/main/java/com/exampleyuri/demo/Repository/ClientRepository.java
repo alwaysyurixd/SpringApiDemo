ea4d6b6d9b50ddaccd18929b3ecff0f726a02c0b
Yuri Carranza <alwaysyurixd@hotmail.com>
2019-08-07 23:20:36 -0500

    Delete

--- src/main/java/com/exampleyuri/demo/Repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exampleyuri.demo.Repository;
-
-import com.exampleyuri.demo.Model.Client;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-
-}
